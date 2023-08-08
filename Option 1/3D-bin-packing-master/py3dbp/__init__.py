4e2e2771d00f98d0ef79895c8ccccc6b24a9ad9e
Anson <110140469+Orronge@users.noreply.github.com>
2023-08-08 18:46:42 +0800

    add import of Axis and aux methods to be tested

--- Option 1/3D-bin-packing-master/py3dbp/__init__.py
+++ Option 1/3D-bin-packing-master/py3dbp/__init__.py
@@ -1,3 +1,4 @@
 from .main import Packer, Bin, Item, Painter
 # Had to add imports for aux methods in order to test them specifically
-from .auxiliary_methods import getLimitNumberOfDecimals, set2Decimal
+from .auxiliary_methods import getLimitNumberOfDecimals, set2Decimal, intersect, rectIntersect
+from .constants import Axis
