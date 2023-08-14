d762672022d884f4cb0f026127f87460742b356e
Anson <110140469+Orronge@users.noreply.github.com>
2023-08-14 09:56:58 +0800

    Created framework for test suite

--- Option 2 - CTC/test_suite.py
+++ Option 2 - CTC/test_suite.py
@@ -3,8 +3,27 @@ import unittest
 
 class TestAux(unittest.TestCase):
 
-    def test_temp(self):
-        return "temp"
+    # Testing Auxiliary Methods
+    def test_get_limit_number_of_decimals(self):
+        return None
+    
+    def test_set_to_decimal(self):
+        return None
+    
+    def test_rect_intersect(self):
+        return None
+    
+    def test_intersect(self):
+        return None
+    
+    def test_stack(self):
+        return None
+    
+    # Testing Item Class Methods
+
+    # Testing Bin Class Methods
+
+    # Testing Packer Class Methods
 
 
 if __name__ == '__main__':
