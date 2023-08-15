83c08db17271b9ab85c204abdca19ab3325f9c98
Anson <110140469+Orronge@users.noreply.github.com>
2023-08-15 17:00:27 +0800

    Add test for add item method for packer class

--- Option 2 - CTC/test_suite.py
+++ Option 2 - CTC/test_suite.py
@@ -555,8 +555,35 @@ class TestAux(unittest.TestCase):
         self.assertEqual(retStr(testPacker.bins), ['300(300x300x300, max_weight:400) vol(27000000.000) item_number(0) filling_ratio(0.000)', '1000(200x500x700, max_weight:20000) vol(70000000.000) item_number(0) filling_ratio(0.000)'])
     
     def test_packerAddItem(self):
-        return
+        # Method to return string value of list of items
+        def retStr(lst):
+            if isinstance(lst, list):
+                res = []
+                for item in lst:
+                    res.append(item.string())
+                return res
+            else:
+                return lst.string()
+            
+        testPacker = Packer()
+        testItem1 = Item('testItem1', 10, 120, 30, 120)
+        testItem2 = Item('testItem2', 100, 10, 60, 100)
+        self.assertEqual(testPacker.total_items, 0)
+        self.assertEqual(retStr(testPacker.unplaced_items), [])
+
+        testPacker.add_item(testItem1)
+        self.assertEqual(testPacker.total_items, 1)
+        self.assertEqual(retStr(testPacker.unplaced_items), ['testItem1(10x120x30, weight: 120) pos([0, 0, 0]) rt(0) vol(36000.000)'])
     
+        testPacker.add_item(testItem2)
+        self.assertEqual(testPacker.total_items, 2)
+        self.assertEqual(retStr(testPacker.unplaced_items), ['testItem1(10x120x30, weight: 120) pos([0, 0, 0]) rt(0) vol(36000.000)', 'testItem2(100x10x60, weight: 100) pos([0, 0, 0]) rt(0) vol(60000.000)'])
+
+        testPacker.add_item('testItem1')
+        self.assertEqual(testPacker.total_items, 3)
+        with self.assertRaises(AttributeError):
+            retStr(testPacker.unplaced_items)
+
     def test_packerPivotDict(self):
         return
     
