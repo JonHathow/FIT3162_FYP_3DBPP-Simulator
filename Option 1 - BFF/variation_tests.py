d953307e12e72d3f4eb47d4eb43f75716d946231
Anson <110140469+Orronge@users.noreply.github.com>
2023-08-25 19:06:57 +0800

    Add framework for testing of variations

---
+++ Option 1 - BFF/variation_tests.py
@@ -0,0 +1,20 @@
+#Testing suite designed to test all of the methods in the auxiliary_methods.py folder in the py3dbp folder
+from py3dbp import rectIntersect, intersect, getLimitNumberOfDecimals, set2Decimal, Item, Bin, Packer, Axis
+from decimal import Decimal
+import decimal
+import unittest
+import numpy as np
+
+class TestAux(unittest.TestCase):
+    
+    def putItem_Variation(self):
+        return
+    
+    def packerPack_Variation(self):
+        return
+
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
