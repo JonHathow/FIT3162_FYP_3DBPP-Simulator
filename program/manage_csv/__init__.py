555cd6b5d8c3baf01a0e0654a9a38bb46229d0e4
Anson <110140469+Orronge@users.noreply.github.com>
2023-08-31 11:59:22 +0800

    Add imports needed for tests

--- program/manage_csv/__init__.py
+++ program/manage_csv/__init__.py
@@ -2,3 +2,6 @@ from .constants import Option, Mode, MENU_INPUT, MENU_INVALID, MENU_END, MENU_BI
 from .read_input_csv import read_input
 from .write_input_bin import write_input_bin
 from .write_input_box import write_input_box
+
+# imports needed for test suite
+from .input_parameters import InputBinParameters, InputBoxParameters
