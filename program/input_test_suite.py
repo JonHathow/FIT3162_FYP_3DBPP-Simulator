1caa50e1c8ef89c0c577f2e27b1eb7029fe3e8c5
Anson <110140469+Orronge@users.noreply.github.com>
2023-08-31 11:56:34 +0800

    create main file for input tests

---
+++ program/input_test_suite.py
@@ -0,0 +1,22 @@
+#Testing suite designed to test all of the methods in the auxiliary_methods.py folder in the py3dbp folder
+from decimal import Decimal
+import unittest
+import numpy as np
+
+class TestAux(unittest.TestCase):
+    
+    # manage_csv tests
+    def test_pack2Bin(self):
+        pass
+
+    # Option1_input tests
+    def test_pack2Bin(self):
+        pass
+
+    # Option2_input tests
+    def test_pack2Bin(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
