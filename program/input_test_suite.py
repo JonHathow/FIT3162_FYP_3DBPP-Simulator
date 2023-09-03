9aa2901b736b6ae3f49cb4ce50cf775a4d103679
Anson <110140469+Orronge@users.noreply.github.com>
2023-09-03 14:18:19 +0800

    Add tests for prompts.py

--- program/input_test_suite.py
+++ program/input_test_suite.py
@@ -1,6 +1,7 @@
 #Testing suite designed to test all of the methods in the auxiliary_methods.py folder in the py3dbp folder
-from manage_csv import InputBinParameters, InputBoxParameters
+from manage_csv import InputBinParameters, InputBoxParameters, prompt_range, prompt_number, prompt_boolean, prompt_integer, get_input
 from decimal import Decimal
+from unittest.mock import patch
 import unittest
 import numpy as np
 
@@ -72,11 +73,130 @@ class TestAux(unittest.TestCase):
         testClass2 = InputBoxParameters("3", False, 'c', 46.78, InputBoxParameters(0, 0, 0, 0, 0, 0, 0), 50, 300)
         self.assertEqual(testClass2.__str__(),"\ntypes: 3\nqty_lo: False, qty_hi: c\ndim_lo: 46.78, dim_hi: \ntypes: 0\nqty_lo: 0, qty_hi: 0\ndim_lo: 0, dim_hi: 0\nwgt_lo: 0, wgt_hi: 0\nlevel_var: False\nloadbear_var: False\nupdown_var: False\nupdown: False\n\nwgt_lo: 50, wgt_hi: 300\nlevel_var: False\nloadbear_var: False\nupdown_var: False\nupdown: False\n")
 
+    # prompts.py
+    @patch('builtins.input')
+    def test_getInput(self, mock_input):
+        mock_input.return_value = "4"
+        self.assertEqual(get_input("height"),"4")
+
+        mock_input.return_value = "four"
+        self.assertEqual(get_input("height"),"four")
+
+        mock_input.return_value = "True"
+        self.assertEqual(get_input("height"),"True")
+
+    @patch('manage_csv.prompts.get_input')
+    def test_promptNumber(self, mock_get_input):
+
+        # input always return string
+
+        mock_get_input.return_value = "4"
+        self.assertEqual(prompt_number("dimension"), float(4))
+
+        mock_get_input.return_value = "4.56"
+        self.assertEqual(prompt_number("dimension"), float(4.56))
+
+        mock_get_input.return_value = "-45"
+        self.assertEqual(prompt_number("dimension"), float(45))
+
+        #                         infinite loop
+        # mock_get_input.return_value = "Four"
+        # self.assertEqual(prompt_number("dimension"), "ERROR: Invalid numeric input.")
+
+        #                         infinite loop
+        # mock_get_input.return_value = "True"
+        # self.assertEqual(prompt_number("dimension"), "ERROR: Invalid numeric input.")
+
+        #                         infinite loop
+        # mock_get_input.return_value = "[42]"
+        # self.assertEqual(prompt_number("dimension"), "ERROR: Invalid numeric input.")
+
+    @patch('manage_csv.prompts.get_input')
+    def test_promptInteger(self, mock_get_input):
+
+        # input always return string
+
+        mock_get_input.return_value = "4"
+        self.assertEqual(prompt_integer("dimension"), 4)
+
+        mock_get_input.return_value = "-45"
+        self.assertEqual(prompt_integer("dimension"), 45)
+
+        """
+                                Infinite Loop
+
+                                possible fix:
+                convert from string input to float, then to int
+        """
+        # mock_get_input.return_value = "4.56"
+        # self.assertEqual(prompt_integer("dimension"), int(4.56))
+
+        #                         infinite loop
+        # mock_get_input.return_value = "Four"
+        # self.assertEqual(prompt_integer("dimension"), "ERROR: Invalid numeric input.")
+
+        #                         infinite loop
+        # mock_get_input.return_value = "True"
+        # self.assertEqual(prompt_integer("dimension"), "ERROR: Invalid numeric input.")
+
+        #                         infinite loop
+        # mock_get_input.return_value = "[42]"
+        # self.assertEqual(prompt_integer("dimension"), "ERROR: Invalid numeric input.")
+
+    @patch('manage_csv.prompts.get_input2')
+    @patch('manage_csv.prompts.get_input')
+    def test_promptRange(self, mock_get_input1, mock_get_input2):
+
+        mock_get_input1.return_value = "1"
+        mock_get_input2.return_value = "10"
+        self.assertEqual(prompt_range("containers needed"), (1,10))
+
+        #                       Infinite Loop
+        # mock_get_input1.return_value = "1.5"
+        # mock_get_input2.return_value = "10.9"
+        # self.assertEqual(prompt_range("containers needed"), 'ERROR: Invalid integer input.')
+
+        #                       Infinite Loop
+        # mock_get_input1.return_value = "Yes"
+        # mock_get_input2.return_value = "No"
+        # self.assertEqual(prompt_range("containers needed"), 'ERROR: Invalid integer input.')
+
+    @patch('manage_csv.prompts.get_input')
+    def test_promptBoolean(self, mock_get_input):
+
+        mock_get_input.return_value = "y"
+        self.assertEqual(prompt_boolean("to pass fit3162"), True)
+
+        mock_get_input.return_value = "Y"
+        self.assertEqual(prompt_boolean("to pass fit3162"), True)
+
+        mock_get_input.return_value = "n"
+        self.assertEqual(prompt_boolean("to pass fit3162"), False)
+
+        mock_get_input.return_value = "N"
+        self.assertEqual(prompt_boolean("to pass fit3162"), False)
+
+        #                       Infinite Loop
+        # mock_get_input.return_value = "yes"
+        # self.assertEqual(prompt_boolean("to pass fit3162"), 'ERROR: Invalid input. Y/N only.')
+
+        #                       Infinite Loop
+        # mock_get_input.return_value = "24"
+        # self.assertEqual(prompt_boolean("to pass fit3162"), 'ERROR: Invalid input. Y/N only.')
+        
+
+    # write_input_bin.py
+    def test_writeInputBin(self):
+        return
+
+    # write_input_box.py
+    def test_writeInputBox(self):
+        return
+    
     # manage_filecount.py
     # need more time to research and implement tests for external files
 
-    # prompts.py
-    # need more time to research and implement tests for prompts
+    
 
     # Option1_input tests
     def test_pack2Bin(self):
