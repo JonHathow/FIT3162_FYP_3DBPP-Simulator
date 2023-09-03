0a0b3d93987f261f1a9c7fb384f6b7fa4b5b450c
Anson <110140469+Orronge@users.noreply.github.com>
2023-09-03 19:05:50 +0800

    change func name for test

--- program/manage_csv/__init__.py
+++ program/manage_csv/__init__.py
@@ -1,7 +1,7 @@
 from .constants import Option, Mode, MENU_INPUT, MENU_INVALID, MENU_END, MENU_BIN_NOTLOADED, FILE_BIN_1, FILE_BOX_1
 from .read_input_csv import read_input
 from .write_input_bin import write_input_bin_func, prompt_input_bins
-from .write_input_box import write_input_box
+from .write_input_box import write_input_box_func, prompt_input_boxes
 
 # imports needed for test suite
 from .input_parameters import InputBinParameters, InputBoxParameters
