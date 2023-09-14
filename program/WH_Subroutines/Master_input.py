a67137a871db1e57bd4cb4a6eb7c9b22ed533bad
JonHathow <jon.yuchern@gmail.com>
2023-09-14 14:57:16 +0800

    Commit Master Input

---
+++ program/WH_Subroutines/Master_input.py
@@ -0,0 +1,23 @@
+""" 
+Master Subroutine to help Warehouse Main manage the other Input Subroutines for the respective algorithms.
+"""
+def handle_input():
+   # ToDo:
+   # 1. Move all common functionality of subroutines to this method
+   # 2. Help coordinate with UI to receive input for subroutines
+   # 3. Invoke relevant subroutine for passing control flow to algorithms.
+   pass
+
+# master Input Main
+def main():
+   flag = False
+   
+   # Welcome Message
+   print("==================================================================")
+   print("This is the Master Input Subroutine Main Program")
+   print("Which is used to test and run the Master Subroutine")
+
+   return None
+
+if __name__ == "__main__":
+   main()
