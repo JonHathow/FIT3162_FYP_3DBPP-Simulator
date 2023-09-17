3863bc8f6ad2c6a1ee3057bbcd65ecde378022bd
cf.le3 <58763304+cf-Le3@users.noreply.github.com>
2023-09-17 20:28:41 +0800

    Add more types to export

--- program/manage_csv/__init__.py
+++ program/manage_csv/__init__.py
@@ -1,4 +1,4 @@
-from .constants import Option, Mode, MENU_INPUT, MENU_INVALID, MENU_END, MENU_BIN_NOTLOADED, FILE_BIN_1, FILE_BOX_1
+from .constants import Option, File, Mode, MENU_INPUT, MENU_INVALID, MENU_END, MENU_BIN_NOTLOADED, MENU_BOX_NOTLOADED, FILE_BIN_1, FILE_BOX_1, HEADER_OUT_1
 from .read_input_csv import read_input
 from .write_input_bin import write_input_bin
 from .write_input_box import write_input_box
