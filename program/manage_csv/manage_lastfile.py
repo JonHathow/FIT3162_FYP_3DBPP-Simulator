51936c698f00c31fe60ace94cd25ff672523c3d7
cf.le3 <58763304+cf-Le3@users.noreply.github.com>
2023-09-17 20:22:45 +0800

    Update name of enum types where utilized
    Reflects changes to constants.py.

--- program/manage_csv/manage_lastfile.py
+++ program/manage_csv/manage_lastfile.py
@@ -1,16 +1,16 @@
 # TODO: Evaluate documentation.
 
 import os
-from .constants import Option, Mode, FOLDER_OUTPUTS_1, FOLDER_OUTPUTS_2, FILE_LASTBIN_1, FILE_LASTBIN_2, FILE_LASTBOX_1, FILE_LASTBOX_2
+from .constants import Option, File, FOLDER_OUTPUTS_1, FOLDER_OUTPUTS_2, FILE_LASTBIN_1, FILE_LASTBIN_2, FILE_LASTBOX_1, FILE_LASTBOX_2
 
-def update_lastfile(filename: str, option: Option, mode: Mode):
+def update_lastfile(filename: str, option: Option, filetype: File):
     
-    if option == Option.OPTION1:
+    if option == Option.OPTION1.value:
         folder_path = FOLDER_OUTPUTS_1
-        file_path   = FILE_LASTBIN_1 if mode == Mode.BIN else FILE_LASTBOX_1
+        file_path   = FILE_LASTBIN_1 if filetype == File.BIN.value else FILE_LASTBOX_1
     else:
         folder_path = FOLDER_OUTPUTS_2
-        file_path   = FILE_LASTBIN_2 if mode == Mode.BIN else FILE_LASTBOX_2
+        file_path   = FILE_LASTBIN_2 if filetype == File.BIN.value else FILE_LASTBOX_2
 
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
