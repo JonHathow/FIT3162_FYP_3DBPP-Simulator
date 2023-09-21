a5159b39e62afa3ad87ce6829f9be81463365145
cf.le3 <58763304+cf-Le3@users.noreply.github.com>
2023-09-22 06:33:49 +0800

    Add documentation

--- program/manage_csv/manage_lastfile.py
+++ program/manage_csv/manage_lastfile.py
@@ -1,10 +1,28 @@
-# TODO: Evaluate documentation.
+""" 
+Functions to read/write to lastBinFile.txt and lastBoxFile.txt files. As their
+names suggest, these files contain the name of the last bin and box CSV files
+read as input so they can be listed in the output summary file.
+"""
 
 import os
 from .constants import Option, File, FOLDER_OUTPUTS_1, FOLDER_OUTPUTS_2, FILE_LASTBIN_1, FILE_LASTBIN_2, FILE_LASTBOX_1, FILE_LASTBOX_2
 
 def update_lastfile(filename: str, option: Option, filetype: File):
-    
+    """
+    Overwrite the name of the last CSV file read from as recorded in
+    a lastFile file.
+
+    filename    - the name of the CSV file that was read from, to be
+                  recorded in the corresponding lastFile file
+
+    option      - specifies whether the function is being called by
+                  Option 1 or Option 2, used to determine file and
+                  folder paths
+
+    filetype    - specifies whether the read file was for bins or boxes,
+                  used to determine file path of lastFile file
+    """
+
     if option == Option.OPTION1.value:
         folder_path = FOLDER_OUTPUTS_1
         file_path   = FILE_LASTBIN_1 if filetype == File.BIN.value else FILE_LASTBOX_1
@@ -20,6 +38,17 @@ def update_lastfile(filename: str, option: Option, filetype: File):
     file.close()
 
 def fetch_lastfile(option: Option, filetype: File):
+    """
+    Fetch the name of the last CSV file read from, as recorded in a
+    lastFile file.
+
+    option      - specifies whether the function is being called by
+                  Option 1 or Option 2, used to determine file and
+                  folder paths
+
+    filetype    - specifies whether the read file was for bins or boxes,
+                  used to determine file path of lastFile file
+    """
 
     if option == Option.OPTION1.value:
         file_path   = FILE_LASTBIN_1 if filetype == File.BIN.value else FILE_LASTBOX_1
