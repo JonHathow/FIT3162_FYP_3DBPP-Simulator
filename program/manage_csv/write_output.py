4073cd4f91005767bce1c107b90db44398d81f4b
cf.le3 <58763304+cf-Le3@users.noreply.github.com>
2023-09-22 06:51:26 +0800

    Add documentation

--- program/manage_csv/write_output.py
+++ program/manage_csv/write_output.py
@@ -1,6 +1,23 @@
+"""
+General use function to write a given set of rows into a specified
+CSV output file. Usable by either option for producing all requisite
+output files.
+"""
+
 import csv
 
-def write_output(filename: str, filecount: int, rows: [str], header: [str]):
+def write_output(filename: str, filecount: int, rows: [[str]], header: [str]):
+    """
+    filename    - name of the CSV file to write to
+
+    filecount   - numeric identifier of the file to prevent writing
+                  over existing files
+
+    row         - list of lists of strings containing data items to
+                  write to the CSV file, one row is one entry
+
+    header      - CSV file header containing labels for each field
+    """
 
     with open(f'{filename}{filecount}.csv', mode = 'w', newline = "") as csvfile:
         writer = csv.writer(csvfile)
