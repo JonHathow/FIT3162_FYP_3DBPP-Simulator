4f07518fc7104a1b0e6bde1df157bddfb63ffac6
cf.le3 <58763304+cf-Le3@users.noreply.github.com>
2023-09-22 08:15:20 +0800

    Write new functions
    The new functions are only responsible for extracting the relevant data
    from the packer object.
    Writing to the CSV file is handled by the new write_output function.
    Added documentation as well.

--- program/write_output_option1.py
+++ program/write_output_option1.py
@@ -1,74 +1,127 @@
-# TODO: Evaluate documentation.
-# NOTE: Assumes we're only using 1 bin!!!
+"""
+Functions to extract relevant data items from a packer class
+(as implemented in Option 1) and format them into rows to be
+printed in a CSV file.
+"""
 
 import os
-import csv
-from manage_csv.constants import Option, File, Mode, FOLDER_OUTPUTS_1, FILE_OUTCOUNT_1, FILE_FITTED_1, FILE_UNFITTED_1, FILE_METRICS_1, HEADER_OUT_1
+from manage_csv.constants import (Option, File, Mode,
+                                  FOLDER_OUTPUTS_1, FILE_OUTCOUNT_1,
+                                  FILE_FITTED_1, FILE_UNFITTED_1, FILE_SUMMARY_1, FILE_OUTBINS_1,
+                                  HEADER_OUT_1, HEADER_SUM_1, HEADER_OUTBINS_1)
 from manage_csv.manage_filecount import fetch_filecount, update_filecount
 from manage_csv.manage_lastfile import fetch_lastfile
-from Option1_package import Packer, Item
+from manage_csv.write_output import write_output
+from Option1_package import Packer
 
-def write_output_boxes(packer: Packer, fitting: Mode, filecount: int):
+def extract_boxes(packer: Packer, mode: Mode):
+    """
+    Extracts data items pertaining to boxes and formats
+    them into rows to be printed in a CSV file.
 
-    if fitting == Mode.FITTED.value:
-        filename = f'{FILE_FITTED_1}{filecount + 1}.csv'
-    else:
-        filename = f'{FILE_UNFITTED_1}{filecount + 1}.csv'
+    packer      - object that handles packing of boxes into
+                  bins within the algorithm implemented in
+                  Option 1
 
-    with open(filename, mode = 'w', newline = "") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(HEADER_OUT_1)
+    mode        - specifies whether fitted or unfitted boxes
+                  are being handled
+    """
 
-        for idx,b in enumerate(packer.bins):
-            items = b.items if fitting == Mode.FITTED.value else packer.unfit_items
+    rows = []
 
-            for item in items:
-                writer.writerow([item.partno, item.name, item.color,
-                                 item.width, item.height, item.depth, 
-                                 float(item.width) * float(item.height) * float(item.depth),
-                                 float(item.weight)])
-                
-            if Mode.UNFITTED.value:
-                break
+    for idx,b in enumerate(packer.bins):
+        items = b.items if mode == Mode.FITTED.value else packer.unfit_items
 
-    csvfile.close()
+        for item in items:
+            rows.append([item.partno,
+                         item.name,
+                         item.color,
+                         item.width,
+                         item.height,
+                         item.depth,
+                         float(item.width) * float(item.height) * float(item.depth),
+                         float(item.weight),
+                         item.position,
+                         item.rotation_type])
+        
+        # Skip remaining bins when handling unpacked items
+        if mode == Mode.UNFITTED.value:
+            break
+
+    return rows
+
+def extract_summary(packer: Packer):
+    """
+    Extracts data items pertaining to bins and the
+    specific run of the simulation as a whole. Two
+    lists are returned, one containing data items
+    relevant to bins and the other relevant to the
+    overall run of the simulation.
+
+    packer      - object that handles packing of boxes into
+                  bins within the algorithm implemented in
+                  Option 1
+    """
+
+    rows_bins   = []
+    rows_summ  = []
+
+    volume_b = 0
+    volume_f = 0
+    volume_u = 0
+
+    for idx,b in enumerate(packer.bins):
+        vol_b       = b.width * b.height * b.depth
+        volume_b    += vol_b
+
+        for item in b.items:
+            vol_f       = float(item.width) * float(item.height) * float(item.depth)
+            volume_f    += vol_f
+
+        rows_bins.append([b.partno,
+                          vol_b,
+                          vol_f,
+                          float(vol_b) - vol_f,
+                          round(vol_f / float(vol_b) * 100, 2),
+                          b.gravity])
+        
+    for item in packer.unfit_items:
+        volume_u += float(item.width) * float(item.height) * float(item.depth)
+
+    rows_summ.append([fetch_lastfile(Option.OPTION1.value, File.BIN.value),
+                      fetch_lastfile(Option.OPTION1.value, File.BOX.value),
+                      volume_b,
+                      volume_f,
+                      float(volume_b) - volume_f,
+                      round(volume_f / float(volume_b) * 100, 2),
+                      volume_u])
     
-def write_output_metrics(packer: Packer, filecount: int):
-    filename = f'{FILE_METRICS_1}{filecount + 1}.txt'
+    return rows_summ, rows_bins
 
-    with open(filename, mode = 'w', newline = "") as file:
-        volume = 0
-        volume_f = 0
-        volume_u = 0
+def output_master(packer: Packer):
+    """
+    Calls various functions to extract relevant data items from
+    packer and passes them into a function to write them into a
+    CSV file.
 
-        for idx,b in enumerate(packer.bins):
-            volume += b.width * b.height * b.depth
-            gravity = b.gravity
-
-            for item in b.items:
-                volume_f += float(item.width) * float(item.height) * float(item.depth)
-
-        for item in packer.unfit_items:
-            volume_u += float(item.width) * float(item.height) * float(item.depth)
-
-        file.write(f'Bin input file:        {fetch_lastfile(Option.OPTION1.value, File.BIN.value)}\n')
-        file.write(f'Box input file:        {fetch_lastfile(Option.OPTION1.value, File.BOX.value)}\n')
-        file.write(f'Bin volume:            {volume}\n')
-        file.write(f'Volume utilised:       {volume_f}\n')
-        file.write(f'Residual volume:       {float(volume) - volume_f}\n')
-        file.write(f'Space utilisation:     {round(volume_f / float(volume) * 100 ,2)}\n')
-        file.write(f'Gravity distribution:  {gravity}\n')
-        file.write(f'Unpacked volume:       {volume_u}\n')
-
-def write_output(packer: Packer):
+    packer      - object that handles packing of boxes into
+                  bins within the algorithm implemented in
+                  Option 1
+    """
 
     if not os.path.exists(FOLDER_OUTPUTS_1):
         os.makedirs(FOLDER_OUTPUTS_1)
 
     filecount = fetch_filecount(FILE_OUTCOUNT_1)
 
-    write_output_boxes(packer, Mode.FITTED.value, filecount)
-    write_output_boxes(packer, Mode.UNFITTED.value, filecount)
-    write_output_metrics(packer, filecount)
+    rows_fitted             = extract_boxes(packer, Mode.FITTED.value)
+    rows_unfitted           = extract_boxes(packer, Mode.UNFITTED.value)
+    rows_summ, rows_bins    = extract_summary(packer)
 
+    write_output(FILE_FITTED_1, filecount + 1, HEADER_OUT_1, rows_fitted)
+    write_output(FILE_UNFITTED_1, filecount + 1, HEADER_OUT_1, rows_unfitted)
+    write_output(FILE_SUMMARY_1, filecount + 1, HEADER_SUM_1, rows_summ)
+    write_output(FILE_OUTBINS_1, filecount + 1, HEADER_OUTBINS_1, rows_bins)
+    
     update_filecount(FILE_OUTCOUNT_1, filecount)
+    
