38098fd8d09fea43b6b003e93dd586c110bcb593
cf.le3 <58763304+cf-Le3@users.noreply.github.com>
2023-09-22 10:55:22 +0800

    Update imports

--- program/manage_csv/__init__.py
+++ program/manage_csv/__init__.py
@@ -2,7 +2,8 @@ from .constants import (Option, File, Mode,
                         MENU_INPUT, MENU_INVALID, MENU_END, MENU_BIN_NOTLOADED, MENU_BOX_NOTLOADED,
                         FILE_BIN_1, FILE_BIN_2, FILE_BOX_1, FILE_BOX_2,
                         FOLDER_OUTPUTS_1, FILE_OUTCOUNT_1, FILE_FITTED_1, FILE_UNFITTED_1, FILE_SUMMARY_1, FILE_OUTBINS_1,
-                        HEADER_OUT_1, HEADER_OUT_2, HEADER_SUM_1, HEADER_SUM_2, HEADER_OUTBINS_1)
+                        FOLDER_OUTPUTS_2, FILE_OUTCOUNT_2, FILE_FITTED_2, FILE_UNFITTED_2, FILE_SUMMARY_2, FILE_OUTBINS_2,
+                        HEADER_OUT_1, HEADER_OUT_2, HEADER_OUTBINS_1, HEADER_OUTBINS_2, HEADER_SUM)
 from .read_input_csv import read_input
 from .write_input_bin import write_input_bin
 from .write_input_box import write_input_box
