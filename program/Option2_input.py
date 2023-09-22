d40545efad8e818b19aef0e87a267daaa1d5746b
cf.le3 <58763304+cf-Le3@users.noreply.github.com>
2023-09-22 08:53:41 +0800

    Expand conditional structure
    Separate option for computing packing has been included.
    Now also checks to ensure boxes have been loaded first before carrying out packing.

--- program/Option2_input.py
+++ program/Option2_input.py
@@ -1,27 +1,29 @@
 """ Handles writing and reading of CSV files for Option 2. """
 
-from manage_csv.constants import Mode, Option, MENU_INPUT, MENU_INVALID, MENU_BIN_NOTLOADED, MENU_END, FILE_BIN_2, FILE_BOX_2
+from manage_csv.constants import File, Option, MENU_INPUT, MENU_INVALID, MENU_BIN_NOTLOADED, MENU_BOX_NOTLOADED, MENU_END, FILE_BIN_2, FILE_BOX_2
 from manage_csv.write_input_bin import write_input_bin
 from manage_csv.write_input_box import write_input_box
 from manage_csv.read_input_csv import read_input
 from Option2_package import Packer, Bin, Item
 
 bins_loaded = False
+boxes_loaded = False
+
 while True:
 
     response = input(MENU_INPUT)
 
     # Write a CSV file for bins.
     if response == "1":
-        write_input_bin(Option.OPTION2)
+        write_input_bin(Option.OPTION2.value)
 
     # Write a CSV file for boxes.
     elif response == "2":
-        write_input_box(Option.OPTION2)
+        write_input_box(Option.OPTION2.value)
 
     # Read a CSV file for bins.
     elif response == "3":
-        bin_params = read_input(FILE_BIN_2, Mode.BIN)
+        bin_params = read_input(FILE_BIN_2, File.BIN.value, Option.OPTION2.value)
 
         if bin_params is not None:
 
@@ -41,22 +43,29 @@ while True:
 
     # Read a CSV file for boxes.
     elif response == "4":
+        item_params = read_input(FILE_BOX_2, File.BOX, Option.OPTION2)
 
-        if not bins_loaded:
-            print(MENU_BIN_NOTLOADED)
+        if item_params is not None:
+
+            for item in item_params:
+                name = item[0]
+                width = float(item[1])
+                depth = float(item[2])
+                height = float(item[3])
+                weight = float(item[4])
+                packer.add_item(Item(name, width, depth, height, weight))
+
+            boxes_loaded = True
+
+    elif response == "5":
+
+        if not bins_loaded and not boxes_loaded:
+            if not bins_loaded:
+                print(MENU_BIN_NOTLOADED)
+            if not boxes_loaded:
+                print(MENU_BOX_NOTLOADED)
 
         else:
-            item_params = read_input(FILE_BOX_2, Mode.BOX)
-
-            if item_params is not None:
-                for item in item_params:
-                    name = item[0]
-                    width = float(item[1])
-                    depth = float(item[2])
-                    height = float(item[3])
-                    weight = float(item[4])
-                    packer.add_item(Item(name, width, depth, height, weight))
-
             packer.pack()
 
     elif response == "0":
