78868e88356286140c9cf56bd3b6e089f092871f
clee0070 <clee0070@student.monash.edu>
2023-09-26 11:31:59 +0000

    Update imports
    Commented out imports for test suite as some types may have been renamed.

--- program/manage_csv/__init__.py
+++ program/manage_csv/__init__.py
@@ -1,10 +1,16 @@
-from .constants import Option, Mode, MENU_INPUT, MENU_INVALID, MENU_END, MENU_BIN_NOTLOADED, FILE_BIN_1, FILE_BOX_1, FILE_BOX_2, PROMPT_TYPE_BOX, FILE_BINCOUNT_1, FILE_BOXCOUNT_1
-from .read_input_csv import read_input, fetch_filename
-from .write_input_bin import write_input_bin_func, prompt_input_bins
-from .write_input_box import write_input_box_func, prompt_input_boxes
+from .constants import (Option, File, Mode,
+                        MENU_INPUT, MENU_INVALID, MENU_END, MENU_BIN_NOTLOADED, MENU_BOX_NOTLOADED,
+                        FILE_BIN_1, FILE_BIN_2, FILE_BOX_1, FILE_BOX_2,
+                        FOLDER_OUTPUTS_1, FILE_OUTCOUNT_1, FILE_FITTED_1, FILE_UNFITTED_1, FILE_SUMMARY_1, FILE_OUTBINS_1,
+                        FOLDER_OUTPUTS_2, FILE_OUTCOUNT_2, FILE_FITTED_2, FILE_UNFITTED_2, FILE_SUMMARY_2, FILE_OUTBINS_2,
+                        HEADER_OUT_1, HEADER_OUT_2, HEADER_OUTBINS_1, HEADER_OUTBINS_2, HEADER_SUM)
+from .read_input_csv import read_input
+from .write_input_bin import write_input_bin
+from .write_input_box import write_input_box
+from .write_output import write_output
 
 # imports needed for test suite
-from .input_parameters import InputBinParameters, InputBoxParameters
-from .prompts import prompt_boolean, prompt_integer, prompt_number, prompt_range, get_input
-from .manage_filecount import update_filecount, fix_filecount, fetch_filecount
-from .constants import PROMPT_LASTFILE_BIN, PROMPT_LASTFILE_BOX, FILE_BOXCOUNT_2, FILE_BIN_2
+# from .input_parameters import InputBinParameters, InputBoxParameters
+# from .prompts import prompt_boolean, prompt_integer, prompt_number, prompt_range, get_input
+# from .manage_filecount import update_filecount, fix_filecount, fetch_filecount
+# from .constants import PROMPT_LASTFILE_BIN, PROMPT_LASTFILE_BOX, FILE_BOXCOUNT_2, FILE_BIN_2
