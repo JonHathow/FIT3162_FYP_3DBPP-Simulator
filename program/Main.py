e08df98a363da29d64e1d6fef96229ffb8caf4e2
yhow0004 <yhow0004@student.monash.edu>
2023-10-10 20:36:56 +0800

    Completed Box Window All

--- program/Main.py
+++ program/Main.py
@@ -9,47 +9,8 @@ Main Program Responsibilities:
 """
 # Imports
 from cuboids import Container, SKU
-from ui import Main_Window, MS_Window
+from UI import Main_Window, MS_Window
 
-"""
-Old Code
-
-class Warehouse():
-
-   def __init__(self) -> None:
-      self.container_details = None
-      self.containers = []
-      self.SKUs = []
-      self.random_seed = 0
-      self.batch_size = 0
-      
-   def set_random_seed(self, seed):
-      self.random_seed = seed
-
-   # To Do - Warehouse Containers can have different fragility class, categories, and max_weight.
-   # Need to see how to standardize and regulate input better.
-
-   def spawn_containers(self, quantity, length, width, height, fragile, category, max_weight):
-      for i in range(quantity):
-         self.containers += Container(length, width, height, fragile, category, max_weight)
-
-# Capture Input
-def handle_input():
-   index = input()
-   flag = True
-
-   if index == "1":
-      print("# Back Bottom-Left Fill Algorithm Selected")
-      flag = True
-   elif index == "2":
-      print("# Corner To Center Algorithm Selected ")
-      flag = True
-   else:
-      print("Invalid algorithm selected. Please try again.")
-      flag = False
-
-   return flag
-"""
 # Main Window
 def run_main_window(m_title, m_geometry):
 
@@ -74,7 +35,7 @@ def run_ms_window(m_title, m_geometry, c_algo):
 
       # Initialize Master Subroutine Window (MSW)
       print("Chosen Algorithm {}".format(c_algo))
-      s_options = ["1. Create New Box (SKU)", "2. Create New Bin", "3. Load Existing Box (SKU)", "4. Load Existing Bin"] 
+      s_options = ["1. Create New Box (SKU)", "2. Create New Bin", "3. Load Existing Box (SKU)", "4. Load Existing Bin", "5. Compute Algorithm"] 
       msw = MS_Window(m_title, m_geometry, c_algo, s_options)
 
       # Retrieve Data and Status
@@ -83,6 +44,18 @@ def run_ms_window(m_title, m_geometry, c_algo):
       print("Back Pressed: {}".format(back_flag))
 
    return chosen_option, back_flag
+
+# Box Window
+def run_box_window():
+   pass
+
+# Bin Window
+def run_bin_window():
+   pass
+
+# Loading CSVs
+def run_load_window():
+   pass
 
 # Main
 def main():
--- program/UI.py
+++ program/UI.py
@@ -17,6 +17,7 @@ Classes Implemented
 # Imports
 from tkinter import *
 from manage_csv.input_parameters import InputBinParameters
+from manage_csv.input_parameters import InputBoxParameters
 
 ##############################################################################
 # 1. General Purpose - Parent Window
@@ -52,7 +53,13 @@ class Parent_Window():
    #--- Getters ---#
    # Extract Data
    def get_data(self):
-      return self.data
+
+      if self.data is not None:
+         return self.data
+      else: 
+         print("No data retrieved from user.")
+         return None
+         
    
    # Get Window
    def get_window(self):
@@ -94,15 +101,20 @@ class Parent_Window():
       return dropdown, select
    
    # --- Utility Methods --- #
-   # Start Window
-   def start_window(self):
+   # Pack Window
+   def pack_window(self):
 
       # Pack and publish
       self.header.pack()
       self.body.pack()
       self.footer.pack(side = 'bottom')
+      return None
+
+   # Start Window
+   def start_window(self):
 
       # Place Window at Center of Screen
+      self.pack_window()
       self.window.eval('tk::PlaceWindow . center')
       self.window.mainloop()
       return None
@@ -149,9 +161,6 @@ class Main_Window(Parent_Window):
    # Create Window Content
    def initialize_content(self):
 
-      # Call Super - Method Override
-      super().initialize_content()
-
       # ----- Header ----- #
       heading = Label(self.header, text="Warehouse 3DBPP Simulator", font=("Arial", 20), pady = 10)
       heading.grid(row=0, column=0)
@@ -180,9 +189,6 @@ class Main_Window(Parent_Window):
    #--- Utility ---#
    # Fetch Algorithm - Override
    def fetch(self):
-
-      # Method Override
-      super().fetch()
 
       # Fetch Data from window
       self.data = self.a_select.get()
@@ -222,9 +228,6 @@ class MS_Window(Parent_Window):
    # Create Window Content
    def initialize_content(self):
 
-      # Call Super - Method Override
-      super().initialize_content()
-
       # ----- Header ----- #
       # Heading Label
       heading = Label(self.header, text="Chosen Algorithm:", font=("Arial", 20), pady = 10)
@@ -240,7 +243,7 @@ class MS_Window(Parent_Window):
       self.options_field.grid(row=0, column=1, padx=10)
    
       # ----- Footer ----- #
-      compute_button = Button(self.footer , text = "Compute" , command = self.fetch, bg = "lime")
+      compute_button = Button(self.footer , text = "Continue" , command = self.fetch, bg = "lime")
       exit_button = Button(self.footer, text = "Back" , command = self.back, bg = "red", fg = "white")
       compute_button.pack(side = 'left')
       exit_button.pack(side = 'left', padx = 20)
@@ -263,9 +266,6 @@ class MS_Window(Parent_Window):
    
    # Fetch Algorithm - Override
    def fetch(self):
-
-      # Method Override
-      super().fetch()
 
       # Fetch Data from window
       self.data = self.o_select.get()
@@ -292,12 +292,9 @@ class Bin_Window(Parent_Window):
     # Create Window Content
    def initialize_content(self):
 
-      # Call Super - Method Override
-      super().initialize_content()
-
       # ----- Header ----- #
       # Heading Label
-      heading = Label(self.header, text="Create New Bin", font=("Arial", 20), pady = 10)
+      heading = Label(self.header, text="Create New Bin CSV", font=("Arial", 20), pady = 10)
       heading.grid(row=0, column=0)
       heading = Label(self.header, text="Enter Bin Details (Positive Units):", font=("Arial", 15), pady = 5)
       heading.grid(row=1, column=0)
@@ -324,12 +321,14 @@ class Bin_Window(Parent_Window):
    # Get Data - Override
    def get_data(self):
 
-      # Method Override
-      super().get_data()
-
       # Return Data in New Format - Quantity, Width, Height, Depth, Capacity.
-      print(self.data)
-      return InputBinParameters(self.data[0], self.data[1], self.data[2], self.data[3], self.data[4])
+      # But check if data was retrieved (is not nothing) first.
+      if self.data is not None:
+          return InputBinParameters(self.data[0], self.data[1], self.data[2], self.data[3], self.data[4])
+      else: 
+         print("No data retrieved from user.")
+         return None
+     
 
    # Check if "Back" button is pressed.
    def get_backflag(self):
@@ -339,8 +338,6 @@ class Bin_Window(Parent_Window):
    # Fetch Algorithm - Override
    def fetch(self):
 
-      # Method Override
-      super().fetch()
       self.data = []
 
       # Fetch Data from window
@@ -375,7 +372,7 @@ class Bin_Window(Parent_Window):
 class Box_Window_O2(Parent_Window):
 
    # Init
-   def __init__(self, title, geometry, chosen_algorithm, entries) -> None:
+   def __init__(self, title, geometry, chosen_algorithm, entries, range_entries) -> None:
 
       # Create GUI Window
       super().__init__(title, geometry)
@@ -384,9 +381,23 @@ class Box_Window_O2(Parent_Window):
       self.c_algorithm = chosen_algorithm
       self.backflag = False
 
-      self.initialize_content
+      # Entries
+      self.entries = entries
+      self.range_entries = range_entries
+
+      # Second Body for Ranges
+      self.header_second = Frame(self.window, pady=5)
+      self.body_second = Frame(self.window, pady=5)
+
+      self.setup_content()
    
    # Create Window Content
+   def setup_content(self):
+
+      self.initialize_content()
+      self.start_window()
+
+   # Create Content Auxiliary Method
    def initialize_content(self):
 
       # Call Super - Method Override
@@ -394,18 +405,28 @@ class Box_Window_O2(Parent_Window):
 
       # ----- Header ----- #
       # Heading Label
-      heading = Label(self.body, text="Chosen Algorithm:", font=("Arial", 20), pady = 10)
+      heading = Label(self.header, text="Chosen Algorithm: {}".format(self.c_algorithm), font=("Arial", 18), pady = 10)
       heading.grid(row=0, column=0)
 
       # Chosen Algorithm
-      c_algorithm = Label(self.header, text=self.c_algorithm, font=("Arial", 15), bg = "Yellow")
-      c_algorithm.grid(row=1, column=0)
+      h_guide = Label(self.header, text= "Please Input Box Details (Positive Numbers):", font=("Arial", 15))
+      h_guide.grid(row=1, column=0)
 
       # ----- Body ----- #
       # Box Variables - Integer Types
-      # Number of Boxes, quantity range = (qty_lo, qty_hi), dimensions range (WHD) = (dim_lo, dim_hi), weight range = (wgt_lo, wgt_hi)
+      # Number of Boxes
       self.labels = self.create_input_labels(self.entries, 0)
       self.fields = self.create_input_fields(self.entries, 1)
+
+      # 2nd Body Guide Text
+      self.b2_guide = Label(self.header_second, text = "Input Number Ranges (Low to High)", font=("Arial", 12))
+      self.b2_guide.grid(row=0, column=0)
+
+      # Range Variables - Integer Types
+      # Ranges - quantity range = (qty_lo, qty_hi), dimensions range (WHD) = (dim_lo, dim_hi), weight range = (wgt_lo, wgt_hi)
+      range_lab, range_field = self.range_input_fields(self.range_entries)
+      self.labels += range_lab
+      self.fields += range_field
    
       # ----- Footer ----- #
       compute_button = Button(self.footer, text = "Create" , command = self.fetch, bg = "lime")
@@ -415,28 +436,251 @@ class Box_Window_O2(Parent_Window):
       clear_button.pack(side = 'left', padx = 20)
       exit_button.pack(side = 'left', padx = 20)
 
-      # Start Window
-      self.start_window()
       return None
+    
+   # --- Getters --- #
+   # Get Data - Override
+   def get_data(self):
+
+      # Return Data in New Format
+      # Number of Boxes, quantity range = (qty_lo, qty_hi), dimensions range (WHD) = (dim_lo, dim_hi), weight range = (wgt_lo, wgt_hi)
+      if self.data is not None:
+          return InputBoxParameters(self.data[0], min(self.data[1], self.data[2]), max(self.data[1], self.data[2]), min(self.data[3], self.data[4]), max(self.data[3], self.data[4]), min(self.data[5], self.data[6]), max(self.data[5], self.data[6]))
+      else: 
+         print("No data retrieved from user.")
+         return None
    
    # Back Button
    def back(self):
       self.backflag = True
       self.destroy_window()
       return None
+   
+   # Pack Window - Override
+   def pack_window(self):
+      
+      # Pack and publish - With Second Body
+      self.header.pack()
+      self.body.pack()
+      self.header_second.pack()
+      self.body_second.pack()
+      self.footer.pack(side = 'bottom')
+
+      return None
+   
+   # Unique Label and Field Creating Method - Creating Ranges (Low, High) Input
+   def range_input_fields(self, range_entries):
+      
+      entries = []
+      fields = []
+
+      for i in range(len(range_entries)):
+
+         # Labels
+         entries.append(Label(self.body_second, text = range_entries[i], padx = 10))
+         entries[i].grid(row = i, column = 0)
+
+         # (Low , High) Tuple
+         low = Entry(self.body_second)
+         low.grid(row = i, column = 1)
+
+         Label(self.body_second, text = " to ").grid(row = i, column = 2)
+
+         high = Entry(self.body_second)
+         high.grid(row = i, column = 3)
+
+         fields.append(low)
+         fields.append(high)
+
+      return entries, fields
+
+   # Fetch Algorithm - Override
+   def fetch(self):
+
+      self.fetch_aux()
+      print(self.data)
+
+      # Destroy window mainloop and pass control back to main
+      self.destroy_window()
+   
+   # Fetch Auxiliary Method
+   def fetch_aux(self):
+
+      self.data = []
+
+      # Fetch Data from window
+      for i in range(len(self.fields)):
+         
+         # Field Ret Val is String Form
+         field_val = self.fields[i].get()
+
+         # Null Values Check
+         if field_val == '':
+            raise Exception("Input must be positive integer, not Null")
+         
+         # Positive Integers Only Check
+         if field_val.isdigit():
+            field_val = abs(int(field_val))
+            self.data.append(field_val)
+         else:
+            raise Exception("Input must be positive integer, not negative integers, or other types like String")
 
 # 6. Create New Box UI - Option 1 - Which has a few more parameters than Option 2
 class Box_Window_O1(Box_Window_O2):
    
    # Init
-   def __init__(self, title, geometry) -> None:
+   def __init__(self, title, geometry, chosen_algorithm, entries, range_entries, bool_entries) -> None:
+
+      # Boolean Variables
+      self.bool_entries = bool_entries
 
       # Create GUI Window
-      super().__init__(self, title, geometry)
-
-      # Box Details - Options 1 and 2 Differ
+      super().__init__(title, geometry, chosen_algorithm, entries, range_entries)
 
       return None
+   
+   # Setup Content - Override
+   def setup_content(self):
+      # Due to the way the command flow works in Box_Window_02, put new frames in the Setup Content Method.
+      # Third Body - Boolean Variables
+      self.body_third = Frame(self.window, pady = 10)
+      self.initialize_content()
+   
+   # Initialize Content - Override
+   def initialize_content(self):
+
+      # Method Override
+      super().initialize_content()
+
+      # Boolean Variables - Integer Types
+      # Booleans (Yes/No) - "Allow Varying Priority Levels", "Allow Varying Loading Orientations", "Allow Upside Down Loading"
+      self.entries, self.bool_buttons = self.bool_input_fields(self.bool_entries)
+      # print(self.bool_buttons[0].cget('text'))
+
+      # 2nd Button - "Allow Varying Loading Orientations" makes it so that 3rd Button - "Allow Upside Down Loading" is default False if No.2 is True
+      self.bool_buttons[1].config(command=self.toggle_activity)
+
+      self.start_window()
+   
+   # Unique Boolean Field Create Method
+   def bool_input_fields(self, bool_entries):
+
+      entries = []
+      bool_buttons = []
+
+      for i in range(len(bool_entries)):
+
+         # Labels
+         entries.append(Label(self.body_third, text = bool_entries[i], padx = 10))
+         entries[i].grid(row = i, column = 0)
+
+         # Boolean Buttons - Have to be created manually for now since list[i].pack() or any other function does not work.
+         bool_button = Button(self.body_third, text="Yes", width=12, relief="raised")
+         bool_button['command']= lambda a=bool_button:self.flip(a)
+         bool_button.grid(row = i, column = 1)
+
+         bool_buttons.append(bool_button)
+      
+      return entries, bool_buttons
+   
+   # Toggle Button Activity - Allow 2nd button to toggle activity of 3rd button
+   def toggle_activity(self):
+
+      button1 = self.bool_buttons[1]
+      button2 = self.bool_buttons[2]
+
+      if button1.config('relief')[-1] == 'sunken':
+        button2.config(state = DISABLED)
+        button2.config(text = "No")
+      elif button1.config('relief')[-1] == 'raised':
+        button2.config(state = NORMAL)
+        button2.config(text = "Yes")
+
+      self.flip(button1)
+
+      pass
+
+   # Flip Boolean Button - Yes/No
+   def flip(self, button):
+      
+      if button.config('relief')[-1] == 'raised':
+        button.config(relief="sunken")
+        button.config(text="No")
+      else:
+        button.config(relief="raised")
+        button.config(text="Yes")
+
+      # print(button.cget('text'))
+
+   # --- Getters --- #
+   # Get Data - Override
+   def get_data(self):
+
+      # Return Data in New Format
+      # Number of Boxes, quantity range = (qty_lo, qty_hi), dimensions range (WHD) = (dim_lo, dim_hi), weight range = (wgt_lo, wgt_hi)
+      if self.data is not None:
+          ibp = InputBoxParameters(self.data[0], min(self.data[1], self.data[2]), max(self.data[1], self.data[2]), min(self.data[3], self.data[4]), max(self.data[3], self.data[4]), min(self.data[5], self.data[6]), max(self.data[5], self.data[6]))
+          
+          # True / Falses - "Allow Varying Priority Levels", "Allow Varying Loading Orientations", "Allow Upside Down Loading" - False if No.2 is True
+          if self.data[-2]: # updown_var
+            self.data[-1] = False # updown
+         
+          ibp.set_option1_params(self.data[-3], self.data[-2], self.data[-1])
+          return ibp
+      else: 
+         print("No data retrieved from user.")
+         return None
+      
+   # Clear - Override
+   def clear(self):
+      # Call Super
+      super().clear()
+
+      # Reset Buttons
+      for button in self.bool_buttons:
+         button.config(relief="raised")
+         button.config(text="Yes")
+
+   # Fetch - Override
+   def fetch(self):
+
+      # Super Call Fetch Aux to not trigger "destroy window" early
+      self.fetch_aux()
+
+      # Fetch Button Bool Values
+      print(self.bool_buttons)
+
+      for i in range(len(self.bool_buttons)):
+
+         b_val = self.bool_buttons[i].cget('text')
+
+         if b_val == "Yes":
+            self.data.append(True)
+         elif b_val == "No":
+            self.data.append(False)
+      
+      print(self.data)
+
+      # Destroy Window
+      self.destroy_window()
+
+   # Fetch Auxiliary - Super Call
+   def fetch_aux(self):
+      return super().fetch_aux()
+   
+   # Pack Window - Override
+   def pack_window(self):
+      
+      # Pack and publish - With Second Body
+      self.header.pack()
+      self.body.pack()
+      self.header_second.pack()
+      self.body_second.pack()
+      self.body_third.pack()
+      self.footer.pack(side = 'bottom')
+
+      return None
+      
 
 ##############################################################################
 # 7. Output_Window
@@ -453,10 +697,27 @@ class Output_Window(Parent_Window):
 # Main
 def main():
    print("This function is to test the ui programs. ")
-   print("This is not the main file. Please run warehouse_main.py instead.")
+   print("This is not the main file. Please run Main.py instead.")
 
+
+   # --- Params --- #
+
+   # Core Params
+   m_title = "Storage Optimization in Automated Fulfilment Centers"
+   m_geometry = "600x400"
+   bin_params = ["Quantity", "Width", "Height", "Depth", "Capacity"]
+
+   # Box Param Option 1 & Option 2
+   # These 3 are Boolean - "Allow Varying Priority Levels", "Allow Varying Loading Orientations", "Allow Upside Down Loading" - Only if No.2 is False.
+   # If No. 2 is True, no. 3 is always False.
+   bparams_normal = ["Number of Boxes"]
+   bparams_ranges = ["Quantity Range", "Dimensions Range", "Weight Range"]
+   bparams_boolean = ["Allow Varying Priority Levels", "Allow Varying Loading Orientations", "Allow Upside Down Loading"]
+
+   # --- Tests --- #
+   # Main Window Test
    """
-   Test Program
+   print("Main Window Test")
    iw = Main_Window(title = "Storage Optimization in Automated Fulfilment Centers", geometry = "500x300")
 
    print("Data That Was Retrieved:")
@@ -464,26 +725,26 @@ def main():
    print(data)
    """
 
-   m_title = "Storage Optimization in Automated Fulfilment Centers"
-   m_geometry = "500x300"
-   bin_params = ["Quantity", "Width", "Height", "Depth", "Capacity"]
-
-   # Box Param Option 1
-   # These 3 are Boolean - "Allow Varying Priority Levels", "Allow Varying Loading Orientations", "Allow Upside Down Loading" - Only if No.2 is False.
-   # If No. 2 is True, no. 3 is always False.
-   b_params_O1 = ["Number of Boxes", "Quantity Range", "Dimensions Range", "Weight Range", "Allow Varying Priority Levels", "Allow Varying Loading Orientations", "Allow Upside Down Loading"]
-   # Box Param Option 2
-   b_params_O2 = ["Number of Boxes", "Quantity Range", "Dimensions Range", "Weight Range"]
-
    # Bin Window Test
    """
+   print("Bin Window Test")
    bw = Bin_Window(m_title, m_geometry, bin_params)
    inputparam = bw.get_data()
    print(inputparam)
    """
-
+ 
    # Box Window Test - Option 2
-   bw_O2 = Box_Window_O2(m_title, m_geometry, b_params_O2)
+   """
+   print("Box Window Test - Option 2")
+   bin_window_2 = Box_Window_O2(m_title, m_geometry, "Option 2 - Best Match Fill", bparams_normal, bparams_ranges)
+   print(bin_window_2.get_data())
+   print(bin_window_2.backflag)
+   """
+
+   # Box Window Test - Option 1
+   print("Box Window Test - Option 1")
+   bin_window_1 = Box_Window_O1(m_title, m_geometry, "Option 1 - Back Bottom Left Fill", bparams_normal, bparams_ranges, bparams_boolean)
+   print(bin_window_1.get_data())
 
    return None
 
