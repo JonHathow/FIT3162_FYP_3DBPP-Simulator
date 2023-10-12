d924985346b9b8f429c0b683ab418c50da47ed64
clee0070 <clee0070@student.monash.edu>
2023-10-12 22:49:35 +0000

    Update imports
    Option 2’s Painter class can now be imported by external scripts.

--- program/Option2_package/__init__.py
+++ program/Option2_package/__init__.py
@@ -1,5 +1,6 @@
 from .bin import Bin
 from .item import Item
 from .packer import Packer
+from .painter import Painter
 from .auxiliary_methods import get_limit_number_of_decimals, set_to_decimal, rect_intersect, intersect, stack
 from .constants import Axis
